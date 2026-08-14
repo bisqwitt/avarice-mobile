572bba8f35c117fb730845ae9b5471fce00ef142
bisqwitt <yuuki.aschwanden04@gmail.com>
2026-08-14 12:46:49 +0200

    use immersive mode

--- android/src/main/java/com/avaricious/android/AndroidLauncher.java
+++ android/src/main/java/com/avaricious/android/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.avaricious.android;
 
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import com.avaricious.Main;
 import com.avaricious.utility.DeviceInfo;
@@ -14,10 +16,32 @@ public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true; // Recommended, but not required.
+        configuration.useImmersiveMode = true;
 
         DeviceInfo deviceInfo = new AndroidDeviceInfo();
         initialize(new Main(deviceInfo), configuration);
+
+        hideSystemUI();
+    }
+
+    private void hideSystemUI() {
+        WindowInsetsController controller = getWindow().getInsetsController();
+
+        if (controller == null) {
+            return;
+        }
+
+        controller.hide(WindowInsets.Type.systemBars());
+
+        controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) hideSystemUI();
     }
 }
--- ios/src/main/java/com/avaricious/IOSLauncher.java
+++ ios/src/main/java/com/avaricious/IOSLauncher.java
@@ -1,24 +1,34 @@
 package com.avaricious;
 
 import com.avaricious.utility.DeviceInfo;
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.avaricious.Main;
 
-/** Launches the iOS (RoboVM) application. */
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIRectEdge;
+
 public class IOSLauncher extends IOSApplication.Delegate {
+
     @Override
     protected IOSApplication createApplication() {
-        IOSApplicationConfiguration configuration = new IOSApplicationConfiguration();
+        IOSApplicationConfiguration configuration =
+            new IOSApplicationConfiguration();
+
+        configuration.screenEdgesDeferringSystemGestures =
+            UIRectEdge.Bottom;
+        configuration.hideHomeIndicator = false;
+
         DeviceInfo deviceInfo = new IOSDeviceInfo();
-        return new IOSApplication(new Main(deviceInfo), configuration);
+        return new IOSApplication(
+            new Main(deviceInfo),
+            configuration
+        );
     }
 
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
+
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
     }
