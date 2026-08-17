d7495a090c1ce8a3677960bae8a0e4629e047260
bisqwitt <yuuki.aschwanden04@gmail.com>
2026-08-17 10:06:36 +0200

    replace Stream with Seq for IOS compatibility

--- core/src/main/java/com/avaricious/components/automations/SlotMachineSpeed.java
+++ core/src/main/java/com/avaricious/components/automations/SlotMachineSpeed.java
@@ -2,6 +2,7 @@ package com.avaricious.components.automations;
 
 import com.avaricious.components.slot.SlotMachine;
 import com.avaricious.components.slot.SlotMachineResultRunner;
+import com.avaricious.utility.Seq;
 
 public class SlotMachineSpeed extends AbstractAutomationUpgrade {
 
@@ -13,7 +14,7 @@ public class SlotMachineSpeed extends AbstractAutomationUpgrade {
     @Override
     void onUpgrade() {
         SlotMachine slotMachine = SlotMachine.I();
-        slotMachine.getReels().forEach(reel -> reel.setSpeed(reel.getSpeed() + 2));
+        Seq.of(slotMachine.getReels()).forEach(reel -> reel.setSpeed(reel.getSpeed() + 2));
         slotMachine.setReelStopStagger(slotMachine.getReelStopStagger() * 0.5f);
         SlotMachineResultRunner.I().setDefaultDelay(SlotMachineResultRunner.I().getDefaultDelay() - 0.05f);
     }
